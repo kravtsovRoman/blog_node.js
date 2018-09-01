5ef5a82790c0b9b33cfbce692ba2af258a18da9e
Roman <rozario91208@gmail.com>
2018-09-01 14:19:30 +0300

    a

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer();
 
 server.on('request', (req, res) => {
-    res.end('Сервер на Node.js');
+    res.end('Seeerver on Node.js');
 });
 
 server.listen(3000, () => console.log('Сервер работает'));

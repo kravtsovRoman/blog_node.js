0c11423e190f37cd9df468eabc086d38c4f84b20
Roman <rozario91208@gmail.com>
2018-09-01 14:38:24 +0300

    server html

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const http = require('http');
 
-const server = http.createServer();
+const html = `
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Node.js</title>
+    </head>
+    <body>
+        <h1>Node.js</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi animi consequuntur inventore enim necessitatibus nihil minima ipsam, laudantium officia aspernatur soluta? Explicabo sint aperiam repudiandae provident repellat libero similique necessitatibus recusandae. Cum rem suscipit minus illo reiciendis quos alias! Ipsa magni voluptatibus odit quos, dicta id explicabo sint pariatur commodi.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi animi consequuntur inventore enim necessitatibus nihil minima ipsam, laudantium officia aspernatur soluta? Explicabo sint aperiam repudiandae provident repellat libero similique necessitatibus recusandae. Cum rem suscipit minus illo reiciendis quos alias! Ipsa magni voluptatibus odit quos, dicta id explicabo sint pariatur commodi.
+        </p>
+    </body>
+</html>
+`;
 
-server.on('request', (req, res) => {
-    res.end('Seeerver on Node.js');
-});
+http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(html);
+}).listen(3000, () => console.log('Сервер работает'));
 
-server.listen(3000, () => console.log('Сервер работает'));
-
+ 

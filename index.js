c9fd65435e11765b4e2d6f893dd713d9edb2e6f1
Roman <rozario91208@gmail.com>
2018-09-01 21:59:03 +0300

    commit

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+const arr = ['text 1', 'text 2', 'text 3' ];
  
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.get('/', (req, res) => {
+  res.render('index', {
+      arr: arr
+  });
+});
  
+app.get('/create', (req, res) => {
+  res.render('create');
+});
+
+app.post('/create', (req, res) => {
+  arr.push(req.body.text);
+  res.redirect('/');
+});
+
+
+
 app.listen(3000, () => console.log('Прослушивается порт :3000'));

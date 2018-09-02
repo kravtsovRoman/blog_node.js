291830787e7b8b8d0b85769e25441765404b44cd
Roman <rozario91208@gmail.com>
2018-09-02 12:01:30 +0300

    database settings

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const arr = ['text 1', 'text 2', 'text 3'];
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    arr: arr
+  });
+});
+
+app.get('/create', (req, res) => {
+  res.render('create');
+});
+
+app.post('/create', (req, res) => {
+  arr.push(req.body.text);
+  res.redirect('/');
+});
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,28 +1,15 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const app = require('./app');
+const database = require('./database');
+const config = require('./config');
 
-const app = express();
-
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
-
-const arr = ['text 1', 'text 2', 'text 3' ];
- 
-app.get('/', (req, res) => {
-  res.render('index', {
-      arr: arr
+database()
+  .then(info => {
+    console.log(`Connected to ${info.host}: ${info.port} / ${info.name}`);
+    app.listen(config.PORT, () =>
+      console.log(`Прослушивается порт ${config.PORT}`)
+    );
+  })
+  .catch(() => {
+    console.error('Unable to conect to database');
+    process.exit(1);
   });
-});
- 
-app.get('/create', (req, res) => {
-  res.render('create');
-});
-
-app.post('/create', (req, res) => {
-  arr.push(req.body.text);
-  res.redirect('/');
-});
-
-
-
-app.listen(3000, () => console.log('Прослушивается порт :3000'));
